49ce4d13a9968128db8b448a1118c7a3ca4c3458
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-13 22:58:40 -0300

    configuração models app ans

--- apps/ans/models.py
+++ apps/ans/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class ans(models.Model):
+    tempoMaximoHoras = models.TimeField('Tempo Maximo')
+    dataInicio = models.DateField('Data Inicio')
+    dataLimite = models.DateField('Data Limite')
+    status = models.CharField('Status', choices=[('ativo', 'Ativo'), ('inativo', 'Inativo')], max_length=10)
