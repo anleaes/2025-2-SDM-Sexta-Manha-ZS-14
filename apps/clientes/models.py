454378d584d39a8f07c9a184ac83ca4ba335f4a3
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-13 22:15:15 -0300

    declaração dos atributos do app clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.CharField('Email', max_length=50)
+    telefone = models.CharField('Telefone', max_length=15)
+    endereco = models.CharField('Endereço', max_length=100)
+    
