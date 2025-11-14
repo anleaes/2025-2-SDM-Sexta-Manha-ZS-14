b3b0e3fb3d54817f32e993da1dc9bc70db66f9f1
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-13 23:00:26 -0300

    configuração do meta e to string do app ans

--- apps/ans/models.py
+++ apps/ans/models.py
@@ -7,3 +7,11 @@ class ans(models.Model):
     dataInicio = models.DateField('Data Inicio')
     dataLimite = models.DateField('Data Limite')
     status = models.CharField('Status', choices=[('ativo', 'Ativo'), ('inativo', 'Inativo')], max_length=10)
+
+    class Meta:
+        verbose_name = 'ans'
+        verbose_name_plural = 'ans'
+        ordering =['id']
+
+    def __str__(self):
+        return f"self.dateInicio - self.dataLimite",
