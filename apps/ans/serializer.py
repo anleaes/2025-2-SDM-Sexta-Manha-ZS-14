c65d9ad6c2d92f724891739b2252b8fee28d5760
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-14 09:56:59 -0300

    novo início

---
+++ apps/ans/serializer.py
@@ -0,0 +1,13 @@
+from .models import Ans
+from rest_framework import serializers
+
+class AnsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ans
+        fields = '__all__'
+        
+        # Para chamar todos os atributos:
+        # fields = '__all__'
+        
+        # Para chamar somentes os atributos de interesse:
+        # fields = ['id','created_on', 'updated_on', 'name', 'description']
