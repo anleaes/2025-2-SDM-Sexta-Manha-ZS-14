a2d9695a7ca818849ea64455706b966ae040679c
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-14 11:25:27 -0300

    ajuste das urls para o makemigrations e migrate do app clientes

--- sacapp/settings.py
+++ sacapp/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'clientes.apps.CategoriesConfig',
+    'clientes.apps.ClientesConfig',
 ]
 
 MIDDLEWARE = [
