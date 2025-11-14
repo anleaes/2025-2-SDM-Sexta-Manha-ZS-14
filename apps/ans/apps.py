cfb538419b458602f7d5416a7ca1429926f7fac5
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-14 10:11:44 -0300

    Limpando repositório e mantendo apenas o README

--- apps/ans/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ans'
--- chamado/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChamadoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chamado'
