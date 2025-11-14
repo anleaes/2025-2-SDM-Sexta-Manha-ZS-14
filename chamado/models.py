d687ad8ac679f30cb6ad6a4014231bb359f1a8b4
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-14 01:37:51 -0300

    Configurando models do chamado

--- chamado/models.py
+++ chamado/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class chamado(models.Model):
+    titulo = models.CharField('Titulo', max_length=100)
+    descricao = models.TextField('Descricao')
+    status = models.CharField('Status', choices=[('aberto', 'Aberto'), ('em_andamento', 'Em Andamento'), ('fechado', 'Fechado')], max_length=15)
+    dataAbertura = models.DateField('Data Abertura', auto_now_add=True)
+    dataFechamento = models.DateField('Data Fechamento', null=True, blank=True) 
+    prioridade = models.CharField('Prioridade', choices=[('baixa', 'Baixa'), ('media', 'Média'), ('alta', 'Alta')], max_length=10)
+    ans = models.ForeignKey('ans.Ans', on_delete=models.CASCADE, verbose_name='ANS')
+  
