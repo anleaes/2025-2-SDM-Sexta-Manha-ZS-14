bb00cb9accdfcb708c1f98158f52d2eaebd32883
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-14 11:11:45 -0300

    ajustando o models(class clientes) e configurando serializer app clientes

--- apps/clientes/serializer.py
+++ apps/clientes/serializer.py
@@ -1,2 +1,13 @@
-from .models import Category
+from .models import Cliente
 from rest_framework import serializers
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+        
+        # Para chamar todos os atributos:
+        # fields = '__all__'
+        
+        # Para chamar somentes os atributos de interesse:
+        # fields = ['id','created_on', 'updated_on', 'name', 'description']
