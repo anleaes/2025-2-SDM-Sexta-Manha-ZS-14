c5b1bf422dc2533649439589b925aee3e015c2dd
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-13 23:18:11 -0300

    configuração do arquivo views imports e class do app ans

--- apps/ans/views.py
+++ apps/ans/views.py
@@ -3,3 +3,7 @@ from rest_framework import viewsets
 from .models import ans
 from .serializer import ansSerializer
 # Create your views here.
+class ansViewSet(viewsets.ModelViewSet):
+    queryset = ans.objects.all()
+    serializer_class = ansSerializer  
+    
