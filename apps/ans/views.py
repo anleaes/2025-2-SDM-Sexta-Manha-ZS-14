c9577f927a965f057c18f680b7c69a984c494f33
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-14 01:21:28 -0300

    configurando pt2 views app ans

--- apps/ans/views.py
+++ apps/ans/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .models import Ans
 from .serializer import AnsSerializer
 # Create your views here.
+class AnsViewSet(viewsets.ModelViewSet):
+    queryset = Ans.objects.all()
+    serializer_class = AnsSerializer  
