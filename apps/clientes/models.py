dad88085384280e68750380ad08100a4ff3b589d
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-14 11:03:56 -0300

    configuração do models do app clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.CharField('Email', max_length=100)
+    telefone = models.CharField('Telefone', max_length=15)
+    endereco = models.CharField('Endereço', max_length=200)
+    
