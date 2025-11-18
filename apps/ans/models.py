57ec1cd6464667ff0dec7d5e1fff322de956fa9c
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-18 09:12:57 -0300

    terminando de configurar models do app ans

--- apps/ans/models.py
+++ apps/ans/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Ans(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Ans'
+        verbose_name_plural = 'Ans'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
