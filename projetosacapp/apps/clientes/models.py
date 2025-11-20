625c0fb6b24ef350b6e6c645577077a9f9204d32
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-19 23:15:02 -0300

    modifiicando models do app clientes pt1

--- projetosacapp/apps/clientes/models.py
+++ projetosacapp/apps/clientes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Clientes(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.EmailField('Email', max_length=100)
+    telefone = models.CharField('Telefone', max_length=15)
+    endereco = models.CharField('Endereço', max_length=200)
