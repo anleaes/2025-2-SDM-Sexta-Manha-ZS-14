6d31edbe1fbbfee7483d3f79c0374fccebfd5b06
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 16:53:49 -0300

    arrumando models empresas

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -4,27 +4,9 @@ from django.core.validators import RegexValidator
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=255)
-    cnpj = models.CharField(
-    max_length=18,
-    unique=True,
-    validators=[
-        RegexValidator(
-            regex=r'^\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2}$',
-            message='CNPJ inválido. Use o formato 00.000.000/0000-00'
-        )
-    ]
-)
+    cnpj = models.CharField(max_length=18,unique=True,validators=[RegexValidator(regex=r'^\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2}$',message='CNPJ inválido. Use o formato 00.000.000/0000-00')])
     endereco = models.CharField(max_length=255)
-    telefone = models.CharField(
-    max_length=15,
-    blank=True,
-    validators=[
-        RegexValidator(
-            regex=r'^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$',
-            message='Informe um telefone válido, ex: (51) 99999-8888.'
-        )
-    ]
-)
+    telefone = models.CharField(max_length=15,blank=True,validators=[RegexValidator(regex=r'^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$',message='Informe um telefone válido, ex: (51) 99999-8888.')])
     emailContato = models.EmailField(max_length=100,unique=True)
 
     class Meta:
