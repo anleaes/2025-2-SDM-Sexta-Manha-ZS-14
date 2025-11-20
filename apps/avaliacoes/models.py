a702db9c6b725c4976b211961ebd3b3980fec7ab
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 12:31:15 -0300

    colocando foreingkeys e importando cliente e atendimento no app avaliacoes

--- apps/avaliacoes/models.py
+++ apps/avaliacoes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from clientes.models import Cliente
+from atendimentos.models import Atendimento
 
 # Create your models here.
+
+class Avaliacao(models.Model):
+    nota = models.CharField('Nota', max_length=20)
+    comentario = models.TextField('Comentario', max_length=500)
+    dataAvaliacao = models.DateField('Data da Avaliacao')
+    clientes = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    atendimento = models.OneToOneField(Atendimento, on_delete=models.CASCADE, related_name="avaliacao")
