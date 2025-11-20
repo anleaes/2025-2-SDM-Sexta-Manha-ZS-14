dc92cdb147970e639b067a7a70bc4bfc59ea643d
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 12:35:53 -0300

    colocando os caminhos nas urls do app avaliacoes

--- projetosacapp/urls.py
+++ projetosacapp/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls', namespace='clientes')),    
-    path('atendimentos/', include('atendimentos.urls', namespace='atendimentos')),    
-   
+    path('atendimentos/', include('atendimentos.urls', namespace='atendimentos')),   
+    path('avaliacoes/', include('avaliacoes.urls', namespace='avaliacoes')), 
 ]
