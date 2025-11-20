7079b0cd525d749e7861f7fbdc916ad1306d9806
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 16:14:02 -0300

    Colocando os from's do views no app departamentos

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Departamento
+from .serializer import DepartamentoSerializer
 # Create your views here.
