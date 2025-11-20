9a5c286e4a37cb7e248e24264f68db94d7082bcb
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 16:37:45 -0300

    configurando models do app empresas

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 # Create your models here.
+
+class Empresa(models.Model):
+    nome = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=20, unique=True)
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(
+    max_length=15,
+    blank=True,
+    validators=[
+        RegexValidator(
+            regex=r'^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$',
+            message='Informe um telefone válido, ex: (51) 99999-8888.'
+        )
+    ]
+)
+    emailContato = models.EmailField(max_length=30,unique=True)
+
+    
