1f21fecc085adf1dac20ccd3b5bca7bbfdc1c0a2
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 16:40:09 -0300

    terminando o serializer do app empresas

--- apps/empresas/serializer.py
+++ apps/empresas/serializer.py
@@ -1,2 +1,7 @@
 from .models import Empresa
 from rest_framework import serializers
+
+class EmpresaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Empresa
+        fields = '__all__'
