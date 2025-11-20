70009e196071c6cc4109fc56f761813fb6548faa
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 11:43:49 -0300

    configurando models do app atendimentos

--- apps/atendimentos/models.py
+++ apps/atendimentos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Atendimento(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
