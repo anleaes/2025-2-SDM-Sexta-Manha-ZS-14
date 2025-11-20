2cd8ef1fcda59a5b02cd7c1b5d5fe7e8341fabf8
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 14:37:50 -0300

    adicionando os paraemtros no app prioridades

--- apps/prioridades/models.py
+++ apps/prioridades/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Prioridade(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    descricao = models.TextField('Descricao', max_length=200)
+    tempoMaximoResposta = models.IntegerField('Tempo Maximo de Resposta (em horas)',[MinValueValidator(1)])
+    tempoMaximoResolucao = models.IntegerField('Tempo Maximo de Resolucao (em horas)',[MinValueValidator(1)])
+    
