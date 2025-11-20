f78618454cecb40d2cd24c938b7b53b743b5b77e
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 16:13:24 -0300

    terminando serializer do app departamentos

--- apps/departamentos/serializer.py
+++ apps/departamentos/serializer.py
@@ -1,2 +1,8 @@
 from .models import Departamento
 from rest_framework import serializers
+
+class DepartamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departamento
+        fields = '__all__'
+        
