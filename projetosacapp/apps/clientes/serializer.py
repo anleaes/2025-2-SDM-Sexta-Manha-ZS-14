882eb506aff32f02c1e200718223799264626fd1
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-19 23:22:58 -0300

    acabando o serializer do app clientes

--- projetosacapp/apps/clientes/serializer.py
+++ projetosacapp/apps/clientes/serializer.py
@@ -1,2 +1,8 @@
 from .models import Clientes
 from rest_framework import serializers # type: ignore
+
+class ClientesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clientes
+        fields = '__all__'
+        
