6f5439a72f0e759608655df8efd1338aadc4913a
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 17:13:43 -0300

    criando o serializer, arrumando o models e configurando serializer do app atendentes

--- apps/atendentes/models.py
+++ apps/atendentes/models.py
@@ -3,7 +3,7 @@ from departamentos.models import Departamento
 # Create your models here.
 NIVEIS_ACESSO = [(1, 'Administrador'),(2, 'Usuário Padrão'),(3, 'Convidado'),]
 
-class Category(models.Model):
+class Atendente(models.Model):
     nome = models.CharField('Nome', max_length=50)
     email = models.TextField('Email', max_length=100)
     nivelAcesso = models.IntegerField('Nível de Acesso',choices=NIVEIS_ACESSO,default=3)
---
+++ apps/atendentes/serializer.py
@@ -0,0 +1,2 @@
+from .models import Atendente
+from rest_framework import serializers
