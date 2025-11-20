f4c106d2b217029ac369eedac1684db2aa9d1de9
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 15:50:57 -0300

    configurações models app chamados

--- apps/chamados/models.py
+++ apps/chamados/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from prioridades.models import Prioridade
+from clientes.models import Cliente
+from django.utils import timezone
 
 # Create your models here.
-class Category(models.Model):
+class Chamado(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=500)
+    status = models.BooleanField('Ativo', default=True)
+    dataAbertura = models.DateTimeField(default=timezone.now)
+    dataFechamento = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    prioridades = models.ForeignKey(Prioridade,on_delete=models.CASCADE)
+    clientes = models.ForeignKey(Cliente, on_delete=models.CASCADE)
