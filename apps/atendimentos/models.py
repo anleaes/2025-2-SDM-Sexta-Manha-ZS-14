f8f719e1fd9c160d84770c7b30c1f03b77cd8a9a
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 11:55:12 -0300

    configuração models app atedimentos

--- apps/atendimentos/models.py
+++ apps/atendimentos/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Atendimento(models.Model):
-    name = models.CharField('Nome', max_length=50)
-    description = models.TextField('Descricao', max_length=100)
+    dataInicio = models.DateTimeField(verbose_name='Data de Início',default=timezone.now)
+    dataFim = models.DateTimeField(verbose_name='Data de Fim',)
+    observacoes = models.TextField(verbose_name='Observações',)
+        
+    class Meta:
+        verbose_name = 'Clientes'
+        verbose_name_plural = 'Clientes'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.id}"
