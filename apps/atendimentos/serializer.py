5c00fc5a97b09180f7271e092af903a1d16ba3be
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 11:56:47 -0300

    terminando de configurar serializer do app atendimento

--- apps/atendimentos/serializer.py
+++ apps/atendimentos/serializer.py
@@ -1,2 +1,7 @@
 from .models import Atendimento
 from rest_framework import serializers
+
+class AtendimentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Atendimento
+        fields = '__all__'
