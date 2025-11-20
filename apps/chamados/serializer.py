1c89b12ff3b5a4d1a6e8f98411688c44f62aee01
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 15:54:55 -0300

    terminando o serializer do app chamados

--- apps/chamados/serializer.py
+++ apps/chamados/serializer.py
@@ -1,2 +1,8 @@
 from .models import Chamado
 from rest_framework import serializers
+
+class ChamadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chamado
+        fields = '__all__'
+        
