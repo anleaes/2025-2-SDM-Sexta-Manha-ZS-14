1fd97025aaeaff307dcec5e15193b19aa6b5fc9b
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 15:55:43 -0300

    Terminando o views do app chamados

--- apps/chamados/views.py
+++ apps/chamados/views.py
@@ -3,3 +3,7 @@ from .models import Chamado
 from rest_framework import viewsets
 from .serializer import ChamadoSerializer
 # Create your views here.
+
+class ChamadoViewSet(viewsets.ModelViewSet):
+    queryset = Chamado.objects.all()
+    serializer_class = ChamadoSerializer  
