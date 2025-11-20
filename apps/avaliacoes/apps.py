1ed98255029e9cbad91f305518fd9951519f95ec
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 12:07:07 -0300

    verbose name do avaliacoes

--- apps/avaliacoes/apps.py
+++ apps/avaliacoes/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AvaliacoesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'avaliacoes'
+    verbose_name = 'Avaliacoes'
