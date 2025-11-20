ccddc311296e512f108675012be62876658e4e62
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 17:09:29 -0300

    configurando models do app atendentes

--- apps/atendentes/models.py
+++ apps/atendentes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from departamentos.models import Departamento
 # Create your models here.
+NIVEIS_ACESSO = [(1, 'Administrador'),(2, 'Usuário Padrão'),(3, 'Convidado'),]
+
+class Category(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.TextField('Email', max_length=100)
+    nivelAcesso = models.IntegerField('Nível de Acesso',choices=NIVEIS_ACESSO,default=3)
+    departamentos = models.ManyToManyField(Departamento, blank=False) 
+    
