7d47215a6750c182dbec478bb9e0af3482d37080
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 12:34:09 -0300

    terminando views do app avaliacoes

--- apps/avaliacoes/views.py
+++ apps/avaliacoes/views.py
@@ -3,3 +3,7 @@ from .models import Avaliacao
 from rest_framework import viewsets
 from .serializer import AvaliacaoSerializer
 # Create your views here.
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer  
