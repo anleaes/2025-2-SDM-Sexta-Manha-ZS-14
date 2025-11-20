efb642c4d487edfb9fb8c87e1686c0ccb942506c
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 11:08:40 -0300

    terminando de configurar models do app clientes

--- projetosacapp/apps/clientes/models.py
+++ projetosacapp/apps/clientes/models.py
@@ -3,5 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    name = models.CharField('Nome', max_length=50)
-    description = models.TextField('Descricao', max_length=100)
+    nome = models.CharField('Nome', max_length=50)
+    email = models.EmailField('Email', max_length=50)
+    telefone = models.CharField('Telefone', max_length=50)
+    endereco = models.CharField('Endereço', max_length=50)
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering =['id']
+
+    def __str__(self):
+        return f"self.nome"   
