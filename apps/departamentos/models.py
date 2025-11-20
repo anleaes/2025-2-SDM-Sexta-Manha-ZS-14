9cb18a4661c192be5c2a67107083ea13c2b89500
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 16:11:04 -0300

    configurando models do app departamentos pt1

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    descricao = models.TextField(blank=True, null=True)
+    telefoneContato = models.CharField(
+    max_length=15,
+    blank=True,
+    validators=[
+        RegexValidator(
+            regex=r'^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$',
+            message='Informe um telefone válido, ex: (51) 99999-8888.'
+        )
+    ]
+)
