e9a56d9e8bf14b822e6299658322e291f677c941
maudelasjavador <mauricio.santosbritto@gmail.com>
2025-11-20 17:11:53 -0300

    terminando models do app atendentes

--- apps/atendentes/models.py
+++ apps/atendentes/models.py
@@ -7,5 +7,13 @@ class Category(models.Model):
     nome = models.CharField('Nome', max_length=50)
     email = models.TextField('Email', max_length=100)
     nivelAcesso = models.IntegerField('Nível de Acesso',choices=NIVEIS_ACESSO,default=3)
-    departamentos = models.ManyToManyField(Departamento, blank=False) 
+    departamentos = models.ManyToManyField(Departamento, blank=False)
+
+    class Meta:
+        verbose_name = 'Departamento'
+        verbose_name_plural = 'Departamentos'
+        ordering =['id']
+
+    def __str__(self):
+        return f'{self.nome} - {self.get_nivelAcesso_display()} - {self.departamentos.count()}'
     
